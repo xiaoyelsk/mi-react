ee7774d14796379f8ca64af675cd28c3658c30a4
xiaoyelsk <1214719003@qq.com>
2018-05-12 19:03:27 +0800

    钩子函数

--- src/components/car/car.js
+++ src/components/car/car.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import $ from 'jquery'
+
 import Footer from '../footer/footer.js'
 
 export default class car extends React.Component{
+    componentDidMount(){
+        $('.car').addClass('ative').siblings('a').removeClass('ative')
+    }
     render(){
         return (
             <div>
--- src/components/users/users.js
+++ src/components/users/users.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import $ from 'jquery'
+
 import Footer from '../footer/footer.js'
 
 export default class users extends React.Component{
+    componentDidMount(){
+        $('.users').addClass('ative').siblings('a').removeClass('ative')
+    }
     render(){
         return (
             <div>
