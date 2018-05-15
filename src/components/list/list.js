62562b455e46e621b7aeeaeb13ff004f15d5a60c
方汉佳 <1690101402@qq.com>
2018-05-15 15:44:49 +0800

    获取后台数据和修改路由切换动画

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -5,7 +5,7 @@ import React from 'react'
 import Footer from '../footer/footer.js'
 
 import $ from 'jquery'
-
+import http from '../../utils/httpclient.js'
 export default class List extends React.Component{
     // 回到分类
     backClassify(){
@@ -13,119 +13,96 @@ export default class List extends React.Component{
     }
     // 跳到详情页:将商品id传给详情页
     toDetails(product_id){
-        this.props.router.push('/goods(/:product_id)');
+        this.props.router.push('/goods/'+product_id);
     }
 
     state = {
-        productData:[
-            {
-                img_url:"//i8.mifile.cn/v1/a1/ba867a66-0670-f246-5b63-1b05bf9d4d66.webp",
-                product_brief:"聚热快，受热匀，适用多种炉具",
-                product_id:"7530",
-                product_name:"知吾煮汤锅 米家定制",
-                product_org_price:"99",
-                product_price:"99",
-                type:'xinpin',
-                type_text:'新品'
-            },
-            {
-                img_url:"//i8.mifile.cn/v1/a1/ba867a66-0670-f246-5b63-1b05bf9d4d66.webp",
-                product_brief:"聚热快，受热匀，适用多种炉具",
-                product_id:"7530",
-                product_name:"知吾煮汤锅 米家定制",
-                product_org_price:"99",
-                product_price:"99",
-                type:'xinpin',
-                type_text:'新品'
-            },
-            {
-                img_url:"//i8.mifile.cn/v1/a1/ba867a66-0670-f246-5b63-1b05bf9d4d66.webp",
-                product_brief:"聚热快，受热匀，适用多种炉具",
-                product_id:"7530",
-                product_name:"知吾煮汤锅 米家定制",
-                product_org_price:"99",
-                product_price:"99",
-                type:'xinpin',
-                type_text:'新品'
-            },
-            {
-                img_url:"//i8.mifile.cn/v1/a1/ba867a66-0670-f246-5b63-1b05bf9d4d66.webp",
-                product_brief:"聚热快，受热匀，适用多种炉具",
-                product_id:"7530",
-                product_name:"知吾煮汤锅 米家定制",
-                product_org_price:"99",
-                product_price:"99",
-                type:'xinpin',
-                type_text:'新品'
-            }
-           
-        ],
-        loveData:[
-            {
-                img_url:'//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/e95ade2750a7fde92369b416c7d3176d.jpg',
-                type:'loveProduct',
-                type:'猜你喜欢'
-            },
-            {
-                img_url:"//i8.mifile.cn/v1/a1/48af122b-362c-dae5-8305-899805faf635!360x360.webp",
-                product_brief:"轻巧长续航",
-                product_id:"7758",
-                product_name:"红米5A 32GB",
-                product_org_price:"699.00",
-                product_price:"699.00",
-                type:'loveProduct',
-                type:'猜你喜欢'
-            },
-            {
-                img_url:"//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/9e8104120fc746ecae87626da4ff25da.jpg",
-                product_brief:"轻巧长续航",
-                product_id:"7758",
-                product_name:"红米5A 32GB",
-                product_org_price:"699.00",
-                product_price:"699.00",
-                type:'loveProduct',
-                type:'猜你喜欢'
-            },
-            {
-                img_url:"//i8.mifile.cn/v1/a1/d5c8ea24-5290-46e0-8064-7634b4cbad70.webp",
-                product_brief:"'6.44'大屏5300mAh大电量",
-                product_id:"10000057",
-                product_name:"小米Max 2 大屏大电量",
-                product_org_price:"1399.00",
-                product_price:"1299.00",
-                type:'loveProduct',
-                type:'猜你喜欢'
-            },
-            {
-                img_url:"//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/d68da7f79cc9800a34b1b48e1a439e44.jpg",
-                product_brief:"'5.99'大屏，4轴光学防抖",
-                product_id:"10000070",
-                product_name:"小米MIX 2",
-                product_org_price:"3299.00",
-                product_price:"2899",
-                type:'loveProduct',
-                type:'猜你喜欢'
-            }
-        ]
+        productData:[],
+        loveData:[]
     }
     componentDidMount(){
         // 获取列表传过来的参数
         let product_type = this.props.params.type;
         console.log(product_type)
-        if(product_type == undefined){
-            $('.main-product').hide()
-        }
+        
+        http.post('getProduct',{}).then(res=>{
+            let data = res.data;
+            // 列表数据
+            let productData = [];
+            let proTemp = [];
+            // 猜你喜欢数据：新品
+            let loveData = [];
+            let loveTemp = [];
+
+            // 电视
+            let aData = [];
+            let aTemp = [];
+            // 智能
+            let bData = [];
+            let bTemp = [];
+            // 路由
+            let cData = [];
+            let cTemp = [];
+            if(res.status){
+                let data = res.data;
+                for(var i=0;i<data.length;i++){
+                    let type_text = data[i].type_text;
+                    if(type_text == product_type){
+                        proTemp.push(data[i]);
+                    }else if(type_text == '新品'){
+                        loveTemp.push(data[i]);
+                    }else if(type_text == '电视'){
+                        aTemp.push(data[i]);
+                    }else if(type_text == '智能'){
+                        bTemp.push(data[i]);
+                    }else if(type_text == '路由'){
+                        cTemp.push(data[i]);
+                    }
+                }
+                // 封一个进行二次数据抽取函数
+                function getData(temp,data){
+                    for(var i =0;i<temp.length;i++){
+                        if(i < 2){
+                            continue;
+                        }else{
+                            data.push(temp[i])
+                        }
+                    }
+                }
+                getData(proTemp,productData);
+                getData(loveTemp,loveData);
+                getData(aTemp,aData);
+                getData(bTemp,bData);
+                getData(cTemp,cData);
+
+                // 判断是否有传参过来
+                if(product_type == undefined){
+                    $('.main-product').hide();
+                    // 随机显示猜你喜欢板块
+                    this.setState({
+                        loveData:loveData.concat(aData,bData,cData)
+                    })
+                    return;
+                }else{
+                    this.setState({
+                        productData,
+                        loveData:loveData.concat(aData,bData,cData)
+                    })
+                }
+            }
+        })
     }
 
     render(){
+        let loveImg = '//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/e95ade2750a7fde92369b416c7d3176d.jpg';
         return (
             <div className="f-list">
-                <ul className="list-header">
+                <ul className="list-header animate-route">
                     <li onClick={this.backClassify.bind(this)}> <i className="fa fa-angle-left" aria-hidden="true"></i> </li>
                     <li><span>分类</span></li>
                     <li><i className="fa fa-search"></i></li>
                 </ul>
-                <div className="list-main">
+                <div className="list-main animate-route">
                     <ul className="main-product">
                         {
                             this.state.productData.map((item,idx)=>{
@@ -144,13 +121,15 @@ export default class List extends React.Component{
                             })
                         }
                     </ul>
+
+                
                     <ul className="main-love">
                         {
                             this.state.loveData.map((item,idx)=>{
                                 return (
                                     <li key={idx} onClick={this.toDetails.bind(this,item.product_id)}>
-                                        <img src={item.img_url}/>
-                                        <div className="love-info">
+                                        <img src={idx == 0 ? loveImg:item.img_url}/>
+                                        <div className="love-info" style={idx == 0 ? {display:'none'}:{display:'block'}}>
                                             <p className="name" style={item.product_name ? {display:'block' } : {display:'none'}}>
                                                 {item.product_name}
                                             </p>
