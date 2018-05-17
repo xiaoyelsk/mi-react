aef407a460c79051682946afdb4e5869cf98a322
wtt <2325758070@qq.com>
2018-05-17 22:12:02 +0800

    城市三级联动

--- src/components/users/shippingAddress/shippingAddress.js
+++ src/components/users/shippingAddress/shippingAddress.js
@@ -2,13 +2,41 @@ import './shippingAddress.scss'
 import React from 'react'
 import $ from 'jquery'
 
-
+import http from '../../../utils/httpclient'
 import {Link}from'react-router'
 
 export default class ShippingAddress extends React.Component{
-    Address(){
-
+    state ={
+        addmap:[]
     }
+   componentDidMount(){
+        var uResult=  window.localStorage.getItem('un');
+
+        http.post('getSite',{username:uResult}).then((res) =>{
+            console.log(res)
+            this.setState({
+                addmap:res.data
+            })
+        })
+   }
+   del(minutemap){
+      
+        console.log(minutemap)
+      
+       http.post('delSite',{minutemap}).then((res)=>{
+        console.log(res)
+        if(res){
+            var uResult=  window.localStorage.getItem('un');
+            http.post('getSite',{username:uResult}).then((res) =>{
+                console.log(res)
+                this.setState({
+                    addmap:res.data
+                })
+            })
+        }
+     })
+   }
+   Address(){}
     render(){
         return (
                 <div className="shippingAddress">
@@ -16,7 +44,25 @@ export default class ShippingAddress extends React.Component{
                             <Link to="/quit"><i className="fa fa-angle-left quit-icon"></i></Link>
                             <input type="button" value="收货地址" className="tuichulogin1"/>  
                         </div>
-                        <div className="SA-main"></div>
+                        <div className="SA-main">
+                            <ul ref="bianli">
+                            {
+                                this.state.addmap.map((item,index)=>{
+                                    return (<li key={index}>
+                                                <div className="shangxin">
+                                                    <p>{item.nickname}{item.phone}</p>
+                                                    <i className="Shanchu" onClick={this.del.bind(this,item.minutemap)}>删除</i>
+                                                </div>
+                                                <div className="shangxia">
+                                                    <span>{item.map}</span>
+                                                    <p  ref="minutemap">{item.minutemap}</p>
+                                                    <i className="fa fa-angle-right Syoujiantou"></i>
+                                                </div>
+                                            </li>)
+                                })
+                            }
+                            </ul>
+                        </div>
                         <div className="SA-foot">
                                 <Link to="/SAconsig">
                                     <input type="button" value="新建地址" className="SA-footAdd" onClick={this.Address.bind(this)}/>
